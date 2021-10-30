bce4ff834e444d619b39b930036b3bc055142f21
luka edilashvili <lukaedilashvili@gmail.com>
2021-10-30 09:38:05 +0400

    initial commit

---
+++ app/src/main/java/com/bruh/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bruh.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adjarabet.myapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
